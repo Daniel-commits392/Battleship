0036326f05c5848819cabff616b55f615499886d
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-08-12 15:50:00 +0300

    add variables and create class user for now

---
+++ 1-Python-Fundamental/battlegame.py
@@ -0,0 +1,30 @@
+#TASK 1: setting ariables
+#Characters
+wizard='Wizard'
+elf='Elf'
+human='Human'
+#Hp
+wizard_hp=70
+elf_hp=100
+human_hp=150
+#damage
+wizard_damage=150
+elf_damage=100
+human_damage=20
+#dragon
+dragon_hp=300
+dragon_damage=50
+
+#TASK 2 : prompting players
+class User:
+    def __init__(self,wizard,elf,human):
+        self.wizard=wizard
+        self.human=human
+        self.elf=elf
+
+    def  choose_character():
+          
+
+        
+      
+
