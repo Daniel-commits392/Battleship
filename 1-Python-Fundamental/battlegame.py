9d2597d53f2ea61e7f070981ee00c2c962932e6c
Daniel-commits392 <coderdaniel6853@gmail.com>
2025-08-15 00:05:14 +0300

    add loop to prompt user to pick character

--- 1-Python-Fundamental/battlegame.py
+++ 1-Python-Fundamental/battlegame.py
@@ -34,17 +34,41 @@ class User:
         self.dragon_damage=dragon_damage
 
     def  choose_character(self):
+       while True:
         print('Choose your character')
         print('For this game, you have to chose one of these characters:')
         print(f'(1) {self.wizard}   HP: {self.wizard_hp},  Damage: {self.wizard_damage} ')
         print(f'(2) {self.elf}      HP: {self.elf_hp} ,    Damage: {self.elf_damage} ')
         print(f'(3) {self.human}    HP: {self.human_hp},   Damage: {self.human_damage}')
-        character_choice=input('Choose your character : ')
-        print(character_choice)
+        
+        character_choice=input('Choose either 1, 2 or 3 : ')
+        if character_choice == '1':
+         self.character=self.wizard
+         self.character_hp=self.wizard_hp
+         self.character_damage=self.wizard_damage
+         print(f'You have chosen {self.character} Your HP:{self.character_hp} Your Damage: {self.character_damage}')
+         break
 
+        elif character_choice=='2':
+           self.character=self.elf
+           self.character_hp=self.elf_hp
+           self.character_damage=self.elf_damage
+           print(f'You have chosen {self.character} Your HP:{self.character_hp} Your Damage: {self.character_damage}')
+           break
+
+        elif character_choice=='3':
+            self.character=self.human
+            self.character_hp=self.human_hp
+            self.character_damage=self.human_damage
+            print(f'You have chosen {self.character} Your HP:{self.character_hp} Your Damage: {self.character_damage}')
+            break
+        else:
+           print('Wrong choice.Choose as instructed') 
+           
+  
+        
 player = User(wizard, elf, human, wizard_hp, elf_hp, human_hp, wizard_damage, elf_damage, human_damage, dragon_hp, dragon_damage)
 player.choose_character()
-
 
         
       
